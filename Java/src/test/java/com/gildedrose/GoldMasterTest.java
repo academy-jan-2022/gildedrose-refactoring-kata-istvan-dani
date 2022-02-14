92c6ea07f2c971c72ad05799554c9fa19f84e4ed
danihra-codurance <daniel.rodriguez@codurance.com>
2022-02-14 17:59:33 +0100

    All edge cases covered

--- Java/src/test/java/com/gildedrose/GoldMasterTest.java
+++ Java/src/test/java/com/gildedrose/GoldMasterTest.java
@@ -11,12 +11,16 @@ public class GoldMasterTest {
         "+10 Dexterity Vest, 0, 20\n" +
         "+15 Dexterity Vest, 0, 0\n" +
         "Aged Brie, 2, 0\n" +
+        "Aged Brie, -1, 0\n" +
         "Elixir of the Mongoose, 5, 7\n" +
         "Sulfuras, Hand of Ragnaros, 0, 80\n" +
         "Sulfuras, Hand of Ragnaros, -1, 80\n" +
         "Backstage passes to a TAFKAL80ETC concert, 15, 20\n" +
+        "Backstage passes to a TAFKAL80ETC concert, 10, 20\n" +
+        "Backstage passes to a TAFKAL80ETC concert, 5, 20\n" +
         "Backstage passes to a TAFKAL80ETC concert, 10, 49\n" +
         "Backstage passes to a TAFKAL80ETC concert, 5, 49\n" +
+        "Backstage passes to a TAFKAL80ETC concert, 0, 50\n" +
         "Conjured Mana Cake, 3, 6\n" +
         "\n" +
         "-------- day 1 --------\n" +
@@ -25,12 +29,16 @@ public class GoldMasterTest {
         "+10 Dexterity Vest, -1, 18\n" +
         "+15 Dexterity Vest, -1, 0\n" +
         "Aged Brie, 1, 1\n" +
+        "Aged Brie, -2, 2\n" +
         "Elixir of the Mongoose, 4, 6\n" +
         "Sulfuras, Hand of Ragnaros, 0, 80\n" +
         "Sulfuras, Hand of Ragnaros, -1, 80\n" +
         "Backstage passes to a TAFKAL80ETC concert, 14, 21\n" +
+        "Backstage passes to a TAFKAL80ETC concert, 9, 22\n" +
+        "Backstage passes to a TAFKAL80ETC concert, 4, 23\n" +
         "Backstage passes to a TAFKAL80ETC concert, 9, 50\n" +
         "Backstage passes to a TAFKAL80ETC concert, 4, 50\n" +
+        "Backstage passes to a TAFKAL80ETC concert, -1, 0\n" +
         "Conjured Mana Cake, 2, 5\n" +
         "\n";
 
@@ -42,12 +50,16 @@ public class GoldMasterTest {
             new Item("+10 Dexterity Vest", 0, 20), //
             new Item("+15 Dexterity Vest", 0, 0), //
             new Item("Aged Brie", 2, 0), //
+            new Item("Aged Brie", -1, 0), //
             new Item("Elixir of the Mongoose", 5, 7), //
             new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
             new Item("Sulfuras, Hand of Ragnaros", -1, 80),
             new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 10, 20),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 5, 20),
             new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
             new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 0, 50),
             // this conjured item does not work properly yet
             new Item("Conjured Mana Cake", 3, 6)};
         var result = new GildedRose(items);
